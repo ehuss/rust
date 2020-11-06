6e65a9ff6e3fb1ee8105e1fc50751f75af3388f9
Eric Huss <eric@huss.org>
2020-11-05 18:59:11 -0800

    Test full build.

--- pretty-std-collections.sh
+++ pretty-std-collections.sh
@@ -2,38 +2,46 @@
 
 set -e
 
-rustup toolchain install nightly --profile=minimal
-rustup default nightly
+# rustup toolchain install nightly --profile=minimal
+# rustup default nightly
 rustc --version
 lldb --version
-SRCROOT=$PWD
-mkdir -p build/test
-cd src/tools/compiletest
-SYSROOT=$(rustc --print=sysroot)
 
+./configure --set rust.jemalloc --set llvm.ninja=false
 for i in {1..100}
 do
     echo "run $i"
-    cargo run -- \
-        --compile-lib-path $SYSROOT/lib \
-        --run-lib-path $SYSROOT/lib/rustlib/x86_64-apple-darwin/lib \
-        --rustc-path $SYSROOT/bin/rustc \
-        --src-base $SRCROOT/src/test/debuginfo \
-        --build-base $SRCROOT/build/test \
-        --stage-id stage1-x86_64-apple-darwin \
-        --mode debuginfo \
-        --target x86_64-apple-darwin \
-        --host x86_64-apple-darwin \
-        --host-rustcflags "-Crpath -O -Cdebuginfo=0 -Zunstable-options" \
-        --target-rustcflags "-Crpath -O -Cdebuginfo=0 -Zunstable-options" \
-        --lldb-python /usr/bin/python3 \
-        --lldb-python-dir $(lldb -P) \
-        --docck-python /usr/bin/python3 \
-        --cc "" \
-        --cxx "" \
-        --cflags "" \
-        --llvm-components "" \
-        --android-cross-path "" \
-        --ignored \
-        pretty-std-collections.rs
+    ./x.py test --stage=1 src/test/debuginfo/pretty-std-collections.rs
 done
+
+# SRCROOT=$PWD
+# mkdir -p build/test
+# cd src/tools/compiletest
+# SYSROOT=$(rustc --print=sysroot)
+
+# for i in {1..100}
+# do
+#     echo "run $i"
+#     cargo run -- \
+#         --compile-lib-path $SYSROOT/lib \
+#         --run-lib-path $SYSROOT/lib/rustlib/x86_64-apple-darwin/lib \
+#         --rustc-path $SYSROOT/bin/rustc \
+#         --src-base $SRCROOT/src/test/debuginfo \
+#         --build-base $SRCROOT/build/test \
+#         --stage-id stage1-x86_64-apple-darwin \
+#         --mode debuginfo \
+#         --target x86_64-apple-darwin \
+#         --host x86_64-apple-darwin \
+#         --host-rustcflags "-Crpath -O -Cdebuginfo=0 -Zunstable-options" \
+#         --target-rustcflags "-Crpath -O -Cdebuginfo=0 -Zunstable-options" \
+#         --lldb-python /usr/bin/python3 \
+#         --lldb-python-dir $(lldb -P) \
+#         --docck-python /usr/bin/python3 \
+#         --cc "" \
+#         --cxx "" \
+#         --cflags "" \
+#         --llvm-components "" \
+#         --android-cross-path "" \
+#         --ignored \
+#         pretty-std-collections.rs
+# done
