27264f1f697efff3287b1846e970050a9d281152
Eric Huss <eric@huss.org>
2026-05-14 09:39:34 -0700

    Test hir-def

--- src/ci/docker/scripts/stage_2_test_set2.sh
+++ src/ci/docker/scripts/stage_2_test_set2.sh
@@ -7,15 +7,13 @@ set -ex
 # When this job partition is run as part of PR CI, skip tidy to allow revealing more failures. The
 # dedicated `tidy` job failing won't block other PR CI jobs from completing, and so tidy failures
 # shouldn't inhibit revealing other failures in PR CI jobs.
-if [ "$PR_CI_JOB" == "1" ]; then
-  echo "PR_CI_JOB set; skipping tidy"
-  SKIP_TIDY="--skip tidy"
-fi
+# if [ "$PR_CI_JOB" == "1" ]; then
+#   echo "PR_CI_JOB set; skipping tidy"
+#   SKIP_TIDY="--skip tidy"
+# fi
 
-../x.py --stage 2 test \
-  ${SKIP_TIDY:+$SKIP_TIDY} \
-  --skip tests \
-  --skip coverage-map \
-  --skip coverage-run \
-  --skip library \
-  --skip tidyselftest
+for i in {1..20}
+do
+  echo Step $i
+  ../x.py --stage 2 test src/tools/rust-analyzer
+done
